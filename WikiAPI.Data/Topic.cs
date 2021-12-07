4f329d36592fca0940112909bcbcbf48a7e027ff
Spencer Marsh <scmarsh421@gmail.com>
2021-12-07 10:33:45 -0500

    Topic.cs made

---
+++ WikiAPI.Data/Topic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiAPI.Data
+{
+    public class Topic
+    {
+        [Key]
+        public int TopicId { get; set; }
+        [Required]
+        public string TopicTitle { get; set; }
+        [Required]
+        public DateTime TopicCreatedAt { get; set; }
+        //[Required]
+        //public Dictionary<String sidebar> Sidebar { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+    }
+}
