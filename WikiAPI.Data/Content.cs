451e6ae29ece078011af9ef8e7b57cda0465e453
Ethan Davis <ethanmd103@gmail.com>
2021-12-07 09:51:51 -0500

    Initial Commit

---
+++ WikiAPI.Data/Content.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiAPI.Data
+{
+    class Content
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime created_at { get; set; }
+        [Required]
+        public string Summary { get; set; }
+        [ForeignKey("Topic")] 
+        public int topicID { get; set; }
+        [Required]
+        public string Topic { get; set; }
+        [Required]
+        public int Author_Id { get; set; }
+
+    }
+}
