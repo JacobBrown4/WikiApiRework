9af93d55909e5c7feb3ab64e8aa3606220ce0f0f
Spencer Marsh <scmarsh421@gmail.com>
2021-12-07 14:28:03 -0500

    Topic Get Service and Controller

---
+++ WikiAPI.Services/TopicService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikiAPI.Data;
+using WikiAPI.Models.Topic;
+
+namespace WikiAPI.Services
+{
+    public class TopicService
+    {
+        private readonly Guid _userId;
+        public TopicService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateTopic(TopicCreate topicmodel)
+        {
+            var entity =
+                new Topic()
+                {
+                    TopicId = topicmodel.TopicId,
+                    TopicTitle = topicmodel.TopicTitle,
+                    Summary = topicmodel.Summary,
+                    TopicCreatedAt = DateTime.Now,
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Topic.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<TopicListItem> GetTopic()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Topic.Where(e => e.AuthorId == _userId)
+                    .Select(
+                        e =>
+                        new TopicListItem
+                        {
+                            TopicId = e.TopicId,
+                            TopicTitle = e.TopicTitle,
+                            TopicCreatedAt = e.TopicCreatedAt
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ WikiAPI/Controllers/TopicController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WikiAPI.Services;
+
+namespace WikiAPI.Controllers
+{
+    [Authorize]
+    public class TopicController : ApiController
+    {
+        private TopicService CreateTopicService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var topicService = new TopicService(userId);
+            return topicService;
+        }
+        public IHttpActionResult Get()
+        {
+            TopicService topicService = CreateTopicService();
+            var topic = new TopicService.GetTopic();
+            return Ok(topic);
+        }
+    }
+}
